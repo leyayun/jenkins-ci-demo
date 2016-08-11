85958fbda9e63ea77c4c5d915f74f8e2abee39db
Aaron.Lee <auneeyy@gmail.com>
2016-08-11 17:31:53 +0800

    clean config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,6 @@
 // Modules
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
 var CopyWebpackPlugin = require('copy-webpack-plugin');
 
 /**
@@ -25,44 +24,19 @@ module.exports = function makeWebpackConfig () {
     app: './src/app.js'
   };
 
-  /**
-   * Output
-   * Reference: http://webpack.github.io/docs/configuration.html#output
-   */
   config.output = {
-    // Absolute output directory
     path: __dirname + '/dist',
-
-    // Output path from the view of the page
-    // Uses webpack-dev-server in development
     publicPath: isProd ? '/' : 'http://localhost:8080/',
-
-    // Filename for entry points
-    // Only adds hash in build mode
     filename: isProd ? '[name].[hash].js' : '[name].bundle.js',
-
-    // Filename for non-entry points
-    // Only adds hash in build mode
     chunkFilename: isProd ? '[name].[hash].js' : '[name].bundle.js'
   };
 
-  /**
-   * Devtool
-   * Reference: http://webpack.github.io/docs/configuration.html#devtool
-   * Type of sourcemap to use per build type
-   */
   if (isProd) {
     config.devtool = 'source-map';
   } else {
     config.devtool = 'eval-source-map';
   }
 
-  /**
-   * Loaders
-   * Reference: http://webpack.github.io/docs/configuration.html#module-loaders
-   * List: http://webpack.github.io/docs/list-of-loaders.html
-   * This handles most of the magic responsible for converting modules
-   */
 
   // Initialize module
   config.module = {
@@ -74,63 +48,26 @@ module.exports = function makeWebpackConfig () {
 
 
 
-  /**
-   * Plugins
-   * Reference: http://webpack.github.io/docs/configuration.html#plugins
-   * List: http://webpack.github.io/docs/list-of-plugins.html
-   */
   config.plugins = [];
-
-  // Reference: https://github.com/ampedandwired/html-webpack-plugin
-  // Render index.html
   config.plugins.push(
     new HtmlWebpackPlugin({
       template: './src/index.ejs',
       inject: 'body'
-    }),
-
-    // Reference: https://github.com/webpack/extract-text-webpack-plugin
-    // Extract css files
-    // Disabled when in test mode or not in build mode
-    new ExtractTextPlugin('[name].[hash].css', {disable: !isProd})
+    })
   )
-
-  if (!isProd) {
-    var OpenBrowserPlugin = require('open-browser-webpack-plugin');
-
-    config.plugins.push(
-      new OpenBrowserPlugin({url: 'http://localhost:8080'})
-    )
-  }
 
   // Add build specific plugins
   if (isProd) {
     config.plugins.push(
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#noerrorsplugin
-      // Only emit files when there are no errors
       new webpack.NoErrorsPlugin(),
-
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#dedupeplugin
-      // Dedupe modules in the output
       new webpack.optimize.DedupePlugin(),
-
-      // Reference: http://webpack.github.io/docs/list-of-plugins.html#uglifyjsplugin
-      // Minify all javascript, switch loaders to minimizing mode
       new webpack.optimize.UglifyJsPlugin(),
-
-      // Copy assets from the public folder
-      // Reference: https://github.com/kevlened/copy-webpack-plugin
       new CopyWebpackPlugin([
         { from: __dirname + '/src/index.html' }
       ])
     )
   }
 
-  /**
-   * Dev server configuration
-   * Reference: http://webpack.github.io/docs/configuration.html#devserver
-   * Reference: http://webpack.github.io/docs/webpack-dev-server.html
-   */
   config.devServer = {
     contentBase: './src',
     stats: 'minimal'
